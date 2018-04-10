6d48ebd545b516e7c0dfc9e506a40354de50bc3b
Venkateshkumar M <Venkateshkumar.M@mrcooper.com>
2018-04-10 09:24:15 +0530

    firstCommit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const css = require('./app.css');
+
+ReactDOM.render(<div>Hello World</div>,document.getElementById('root'))
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+
+module.exports ={
+    entry:"./src/app.js",
+    output: {
+        path: path.resolve(__dirname+'/dist/'),
+        filename: 'app.bundle.js'
+    },
+    devServer:{
+        contentBase: path.join(__dirname,"/dist"),
+        compress: true,
+        port: 9000,
+        stats: "errors-only",
+        open: true
+    },
+    module:{
+        rules: [{
+            test:/\.css$/, use:ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader",
+                publicPath: "/dist"
+            })
+                
+        },
+        {
+            test:/\.js$/, use:'babel-loader', exclude: /node_modules/
+        }
+    ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Project',   
+        hash: true,
+        template: './src/index.html'
+    }),
+    new ExtractTextPlugin({
+        filename: "app.css",
+        disable: false,
+        allChunks: true
+    })
+]    
+}
